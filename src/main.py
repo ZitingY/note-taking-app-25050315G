f41f5baf609664fb7315aed78243aea8a37c6f5d
YANG Ziting <25050315g@connect.polyu.hk>
2025-10-19 12:54:09 +0000

    connect with supabase

--- src/main.py
+++ src/main.py
@@ -8,7 +8,12 @@ from flask_cors import CORS
 from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.note import note_bp
+from src.routes.translate import translate_bp
+from src.routes.generate import generate_bp
 from src.models.note import Note
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
@@ -19,14 +24,17 @@ CORS(app)
 # register blueprints
 app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(note_bp, url_prefix='/api')
+app.register_blueprint(translate_bp, url_prefix='/api')
+app.register_blueprint(generate_bp, url_prefix='/api')
 # configure database to use repository-root `database/app.db`
-ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DB_PATH = os.path.join(ROOT_DIR, 'database', 'app.db')
+# 11ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+# 11DB_PATH = os.path.join(ROOT_DIR, 'database', 'app.db')
 # ensure database directory exists
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+# 11os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_PATH}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 with app.app_context():
     db.create_all()
